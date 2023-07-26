4ee1e660c6041c0239429210a61b89fe4692073c
Hari-guptha <harihariguptha@gmail.com>
2023-07-27 01:43:15 +0530

    final work

--- src/features/Socket.jsx
+++ src/features/Socket.jsx
@@ -1,61 +1,84 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import io from 'socket.io-client';
-import Webcam from 'react-webcam';
 import { useDispatch } from 'react-redux';
 import { status } from '../features/color';
 
 const socket = io('http://localhost:5000'); // Replace with your Flask server URL
 
 function Soc() {
-    const dispatch = useDispatch();
-    const [emotionLabel, setEmotionLabel] = useState('');
-    const [isSending, setIsSending] = useState(false); // State to track if a feed is currently being sent
+  const constraints = { video: true, audio: false };
+  const dispatch = useDispatch();
+  const [emotionLabel, setEmotionLabel] = useState('');
+  const [isSending, setIsSending] = useState(false);
 
-    // Create a reference to the Webcam component
-    const webcamRef = React.useRef(null);
+  // Reference to the video element
+  const videoRef = useRef(null);
 
-    // Dispatch the initial emotion label value to the Redux store when the component mounts
-    useEffect(() => {
-        dispatch(status({ emotion: emotionLabel }));
-    }, []);
+  useEffect(() => {
+    // Function to capture frames from the webcam and send to the server
+    const sendWebcamFeed = () => {
+      if (!isSending && videoRef.current) {
+        setIsSending(true);
 
-    useEffect(() => {
-        // Function to capture frames from the webcam and send to the server
-        const sendWebcamFeed = () => {
-            if (!isSending) {
-                setIsSending(true); // Set the sending state to true to avoid sending multiple feeds simultaneously
+        // Get the video element and its canvas
+        const videoElement = videoRef.current;
+        const canvas = document.createElement('canvas');
+        canvas.width = videoElement.videoWidth;
+        canvas.height = videoElement.videoHeight;
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
 
-                const canvas = webcamRef.current.getCanvas(); // Get the canvas element from the Webcam component
-                if (canvas) {
-                    const dataURL = canvas.toDataURL('image/jpeg'); // Convert canvas data to a data URL
-                    socket.emit('video_feed_from_client', dataURL); // Send data URL (video feed) to the server
-                }
+        const dataURL = canvas.toDataURL('image/jpeg'); // Convert canvas data to a data URL
+        socket.emit('video_feed_from_client', dataURL); // Send data URL (video feed) to the server
 
-                setIsSending(false); // Reset the sending state to false after sending the feed
-            }
-        };
+        setIsSending(false);
+      }
+    };
 
-        // Set up interval to capture and send frames at regular intervals (e.g., every 500ms)
-        const intervalId = setInterval(sendWebcamFeed, 500);
+    const intervalId = setInterval(sendWebcamFeed, 500);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isSending]);
 
-        // Clean up the interval when the component unmounts
-        return () => {
-            clearInterval(intervalId);
-        };
-    }, [isSending]);
-
-    // Listen for server response (label) from the Flask server
+  // Listen for server response (label) from the Flask server
+  useEffect(() => {
     socket.on('emotion', (data) => {
-        console.log(data);
-        setEmotionLabel(data); // Set the received label to update the state
-        dispatch(status({ emotion: data })); // Dispatch the updated emotion label to the Redux store
+      console.log(data);
+      setEmotionLabel(data);
+      dispatch(status({ emotion: data }));
     });
+  }, [dispatch]);
 
-    return (
-        <div>
-            <Webcam style={{ width: '50px',position:"absolute",marginLeft:"1000px" }}  ref={webcamRef} />
-        </div>
-    );
+  // Handle webcam stream
+  useEffect(() => {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia(constraints)
+        .then((stream) => {
+          // Update the video source once the stream is available
+          if (videoRef.current) {
+            videoRef.current.srcObject = stream;
+          }
+        })
+        .catch((error) => {
+          console.error('Error accessing the camera:', error);
+        });
+    } else {
+      console.error('getUserMedia API is not supported.');
+    }
+  }, [constraints]);
+
+  // Apply CSS to hide the video element
+  const videoStyle = {
+    display: 'none',
+  };
+
+  return (
+    <div>
+      {/* Use the video element to display the webcam feed */}
+      <video ref={videoRef} autoPlay playsInline style={videoStyle} />
+    </div>
+  );
 }
 
 export default Soc;
